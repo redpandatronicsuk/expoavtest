4d2d2091e42ee7f7ffb03821530d87cb6655aaf3
Dominik Beste <dominik.beste@gmail.com>
2019-05-17 12:46:39 +0200

    updates

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from 'rea
 import React, { Component } from 'react'
 
 import { Audio } from 'expo-av'
+import MusicControl from 'react-native-music-control'
 
 const demoAudio = require('./assets/demo.mp3')
 
@@ -15,6 +16,7 @@ export default class App extends Component {
 
   componentDidMount = async () => {
     await this.setupAudio()
+    this.setupLockScreenControls()
   }
 
   setupAudio = async () => {
@@ -31,20 +33,78 @@ export default class App extends Component {
     })
     this.soundObj = await Audio.Sound.createAsync(
       demoAudio,
-      { shouldPlay: false }
+      { shouldPlay: false },
+      ps => {
+        MusicControl.updatePlayback({
+          state: this.state.isAudioPlaying ? MusicControl.STATE_PLAYING : MusicControl.STATE_PAUSED, // (STATE_ERROR, STATE_STOPPED, STATE_PLAYING, STATE_PAUSED, STATE_BUFFERING)
+          speed: 1, // Playback Rate
+          elapsedTime: (ps.positionMillis || 0) * .001 // (Seconds)
+        })
+      }
     )
+    this.setupLockScreenControls(this.soundObj.status.durationMillis * .001)
     this.setState({ isAudioReady: true })
   }
 
-  togglePlayPause = async () => {
+  setupLockScreenControls = duration => {
+    MusicControl.enableBackgroundMode(true)
+    MusicControl.handleAudioInterruptions(true)
+
+    // Basic Controls
+    MusicControl.enableControl('play', true)
+    MusicControl.enableControl('pause', true)
+    MusicControl.enableControl('stop', false)
+    MusicControl.enableControl('nextTrack', false)
+    MusicControl.enableControl('previousTrack', false)
+
+    // Changing track position on lockscreen
+    MusicControl.enableControl('changePlaybackPosition', true)
+
+    // Seeking
+    MusicControl.enableControl('seekForward', false)
+    MusicControl.enableControl('seekBackward', false)
+    MusicControl.enableControl('skipForward', false)
+    MusicControl.enableControl('skipBackward', false)
+
+    // iOS Specific Options
+    MusicControl.enableControl('enableLanguageOption', false)
+    MusicControl.enableControl('disableLanguageOption', false)
+
+    MusicControl.on('play', this.play)
+    MusicControl.on('pause', this.pause)
+
+    MusicControl.setNowPlaying({
+      title: 'Billie Jean',
+      artwork: 'https://i.imgur.com/e1cpwdo.png', // URL or RN's image require()
+      artist: 'Michael Jackson',
+      album: 'Thriller',
+      genre: 'Post-disco, Rhythm and Blues, Funk, Dance-pop',
+      duration, // (Seconds)
+      description: '', // Android Only
+      color: 0xFFFFFF, // Notification Color - Android Only
+      date: '1983-01-02T00:00:00Z', // Release Date (RFC 3339) - Android Only
+      rating: 84, // Android Only (Boolean or Number depending on the type)
+      notificationIcon: 'my_custom_icon' // Android Only (String), Android Drawable resource name for a custom notification icon
+    })
+  }
+
+  togglePlayPause = () => {
     const { isAudioPlaying } = this.state
     if (isAudioPlaying) {
-      await this.soundObj.sound.pauseAsync()
-      this.setState({ isAudioPlaying: false })
+      this.pause()
     } else {
-      await this.soundObj.sound.playAsync()
-      this.setState({ isAudioPlaying: true })
+      this.play()
     }
+  }
+
+  play = async () => {
+    await this.soundObj.sound.playAsync()
+    this.setState({ isAudioPlaying: true })
+  }
+
+  pause = async () => {
+    await this.soundObj.sound.pauseAsync()
+    this.setState({ isAudioPlaying: false })
   }
 
   render() {
--- android/app/src/main/java/com/avtest/MainApplication.java
+++ android/app/src/main/java/com/avtest/MainApplication.java
@@ -3,6 +3,7 @@ package com.avtest;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.tanguyantoine.react.MusicControl;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -37,6 +38,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MusicControl(),
           new ModuleRegistryAdapter(mModuleRegistryProvider)
       );
     }
